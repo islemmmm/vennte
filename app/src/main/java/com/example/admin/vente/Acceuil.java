4ee29101618c40121f144ce01facfc33164b536a
ESLEMM <islemkhemiri09@gmail.com>
2018-01-11 00:53:39 +0000

    isll

---
+++ app/src/main/java/com/example/admin/vente/Acceuil.java
@@ -0,0 +1,42 @@
+
+package com.example.admin.vente;
+import android.Manifest;
+
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.net.Uri;
+import android.content.Intent;
+
+public class Acceuil extends AppCompatActivity
+{
+
+        Button b = (Button) findViewById(R.id.buttonCall);
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_acceuil);
+            b.setOnClickListener(new View.OnClickListener()
+            {
+                public void onClick(View arg0)
+                {
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("95201478"));
+
+                    if (ActivityCompat.checkSelfPermission(Acceuil.this,
+                            Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+                    {
+                        return;
+                    }
+                    startActivity(callIntent);
+
+                }
+            })
+            ; }
+    }
